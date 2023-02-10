18cd864f714569333e5b78b3198756513a742015
Vetle Skaar <vetleskaar99@gmail.com>
2023-02-10 09:42:26 +0100

    #17 feat: Adds 'app' folder for components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/app/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <div>
       <h1>Strev</h1>
+      <Navbar />
     </div>
   )
 }
