0c46be4a099632c7ec1b7e12e082a451d979dc87
vetls <vetls@stud.ntnu.no>
2023-03-10 12:59:24 +0100

    fix(#61): Consistent login FINALLY fixed

---
+++ context/AuthContext.tsx
@@ -0,0 +1,81 @@
+'use client'
+import React, { useEffect, useMemo} from 'react';
+import { useRouter } from "next/navigation";
+import { User as FirebaseUser } from "firebase/auth"
+import {
+    onAuthStateChanged,
+    getAuth,
+} from 'firebase/auth';
+import { firebase_app } from 'lib/firebase';
+
+
+//const auth = getAuth(firebase_app);
+
+type ContextProps = {
+    user: FirebaseUser | null;
+    loading: boolean;
+}
+
+export const useAuthContext = () => {
+    const context = React.useContext(AuthContext);
+
+    if (context === undefined) {
+        throw new Error(
+          "useFirebaseAuth must be used within a FirebaseAuthProvider"
+        );
+    }
+
+    return context;
+}
+
+export const AuthContext = React.createContext<Partial<ContextProps>>({});
+
+export const AuthProvider = ({ children }: any) => {
+    const [user, setUser] = React.useState(null as FirebaseUser | null);
+    const [loadingAuth, setLoadingAuth] = React.useState(true);
+
+    const userValue = useMemo(() => ({user, loadingAuth}), [user, loadingAuth]);
+
+    //const router = useRouter();
+
+    
+
+    const auth = getAuth(firebase_app);
+
+    //TODO Fix implementering av kommentert kode om nødvendig
+
+    /*const isLoggedIn = !!auth.currentUser;
+
+    React.useEffect(() => {
+        if (isLoggedIn) {
+            router.push("/logg-inn")
+        }
+        
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);*/
+
+    useEffect(() => {
+        auth.onAuthStateChanged((user: any) => {
+            setUser(user);
+            console.log(user);
+            setLoadingAuth(false);
+        });
+    }, []);
+
+    return (
+        <AuthContext.Provider value={ userValue }>
+            {loadingAuth ? <div>Loading...</div> : children}
+        </AuthContext.Provider>
+    );
+};
+
+export default {AuthProvider, useAuthContext}
