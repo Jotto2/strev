40b16f943876482df03bd34a97184f55419f0566
markubre <markubre@stud.ntnu.no>
2023-03-15 09:30:51 +0100

    #66 feat: streak component

--- app/page.tsx
+++ app/page.tsx
@@ -14,10 +14,13 @@ import "styles/globals.css";
 import Navbar from "components/Navbar";
 import StreakComponent from "app/streak/StreakComponent";
 export default function page() {
+
+
+
   return (
     <div className="pb-32 ">
         <div className="flex center ">
-          <StreakComponent count={0}/>
+          <StreakComponent />
          </div>
       <div className="grid place-items-center pt-80">
         <h1 className="text-3xl">Velkommen til Strev</h1>
--- app/streak/StreakComponent.tsx
+++ app/streak/StreakComponent.tsx
@@ -1,11 +1,56 @@
-import { useState, useEffect } from 'react'
+import { async } from '@firebase/util'
+import { useAuthContext } from 'context/AuthContext'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import { firestoreDB } from 'lib/firebase'
+import { useState, useEffect, use } from 'react'
 import { GoFlame } from "react-icons/go"
-import Popup from 'reactjs-popup'
-interface Props {
-  count: number
-}
-export default function StreakComponent({ count }: Props) {
-  const [isVisible, setIsVisible] = useState(true)
+
+export default function StreakComponent() {
+  const [isVisible, setIsVisible] = useState(false)
+  const [dateArray, setDateArray] = useState([])
+  const [displayArray, setDisplayArray] = useState([])
+  const {user} = useAuthContext()
+  
+  async function getDates() {
+    const myCollection = collection(firestoreDB, "users", user.uid)
+    const querySnapshot = await getDocs(query(myCollection, where ("uid","==",user.uid)));
+    const myArray = querySnapshot.docs.map((doc) => {
+      return doc.data().streakArray; // Add ID to data object
+    });
+    console.log(myArray);
+    setDateArray(myArray);
+  }
+
+  const updateBackend = (arg: any[]) =>  {
+    //TODO Send til backend
+  }
+   
+  const runLogic = () => {
+    let loginArray = dateArray;
+
+    const lastLogin: Date = loginArray[loginArray.length - 1];
+    const today = new Date();
+
+      if (loginArray.length > 0) {
+      if (lastLogin.getFullYear() == today.getFullYear() && lastLogin.getMonth() == today.getMonth()) {
+        if (lastLogin.getDay() == today.getDay() -1) {
+          loginArray.push(today);
+        } else if (lastLogin.getDay() !== today.getDay()) {
+          loginArray.splice(0);
+          loginArray.push(today);
+        }
+      } else {
+        loginArray.splice(0);
+        loginArray.push(today);
+      }
+    } else {
+      loginArray.push(today);
+    }
+    setDisplayArray(loginArray);
+    updateBackend(loginArray);
+  }
+  
+
   useEffect(() => {
     const timer1 = setTimeout(() => {
       setIsVisible(true)
@@ -18,14 +63,25 @@ export default function StreakComponent({ count }: Props) {
       clearTimeout(timer2)
     }
   }, [])
+
+  useEffect(() => {
+    getDates();
+  }, [])
+
+  useEffect(() => {
+    runLogic()
+  },[dateArray])
+
   return (
-    <Popup open={isVisible}>
-      <div className="bg-salmon rounded-2xl max-w-md mx-auto grid grid-cols-2 gap-2 p-3 relative shadow-md ">
+    <div className="fixed top-0 left-1/2 transform -translate-x-1/2 mt-16">
+    {isVisible && (
+      <div className="bg-salmon rounded-2xl max-w-md mx-auto grid grid-cols-2 gap-2 p-3 relative shadow-md ease-in-out">
         <div className="flex items-center justify-center w-10 h-10">
           <GoFlame size={24} color="white"/>
         </div>
-        <span className="flex items-center justify-center text-white text-lg font-medium ml-1 absolute left-16 top-5">{count}</span>
+        <span className="flex items-center justify-center text-white text-lg font-medium ml-1 absolute left-16 top-5">{displayArray.length}</span>
       </div>
-    </Popup>
+    )}
+  </div>
   )
 }
