0b73af144c45cce6c741ef9e38cf507e389c4ec4
Vetle Skaar <vetleskaar99@gmail.com>
2023-03-23 08:53:14 +0100

    fix AuthContext

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,76 +1,104 @@
 'use client'
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo} from 'react';
 import { useRouter } from "next/navigation";
 import { User as FirebaseUser } from "firebase/auth"
 import {
-  onAuthStateChanged,
-  getAuth,
+    onAuthStateChanged,
+    getAuth,
 } from 'firebase/auth';
-import { firebase_app } from 'lib/firebase';
+import { firebase_app, firestoreDB } from 'lib/firebase';
+import { doc, collection, getDoc, setDoc } from 'firebase/firestore';
 import { InfinitySpin } from 'react-loader-spinner';
-import Navbar from 'components/Navbar';
+
 
 //const auth = getAuth(firebase_app);
 
 type ContextProps = {
-  user: FirebaseUser | null;
-  loading: boolean;
+    user: FirebaseUser | null;
+    loading: boolean;
 }
 
 export const useAuthContext = () => {
-  const context = React.useContext(AuthContext);
+    const context = React.useContext(AuthContext);
 
-  if (context === undefined) {
-    throw new Error(
-      "useFirebaseAuth must be used within a FirebaseAuthProvider"
-    );
-  }
+    if (context === undefined) {
+        throw new Error(
+          "useFirebaseAuth must be used within a FirebaseAuthProvider"
+        );
+    }
 
-  return context;
+    return context;
 }
 
 export const AuthContext = React.createContext<Partial<ContextProps>>({});
 
 export const AuthProvider = ({ children }: any) => {
-  const [user, setUser] = React.useState(null as FirebaseUser | null);
-  const [loadingAuth, setLoadingAuth] = React.useState(true);
+    const [user, setUser] = React.useState(null as FirebaseUser | null);
+    const [loadingAuth, setLoadingAuth] = React.useState(true);
 
-  const userValue = useMemo(() => ({ user, loadingAuth }), [user, loadingAuth]);
+    const userValue = useMemo(() => ({user, loadingAuth}), [user, loadingAuth]);
 
-  //const router = useRouter();
+    //const router = useRouter();
 
-  const auth = getAuth(firebase_app);
+    
 
-  //TODO Fix implementering av kommentert kode om nødvendig
+    const auth = getAuth(firebase_app);
 
-  /*const isLoggedIn = !!auth.currentUser;
+    //TODO Fix implementering av kommentert kode om nødvendig
 
-  React.useEffect(() => {
-      if (isLoggedIn) {
-          router.push("/logg-inn")
+    /*const isLoggedIn = !!auth.currentUser;
+
+    React.useEffect(() => {
+        if (isLoggedIn) {
+            router.push("/logg-inn")
+        }
+        
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+
+        return () => unsubscribe();
+    }, []);*/
+
+    async function storeUserData(user) {
+      console.log("storeUserData");
+      event.preventDefault(); // Stop page reload
+      const userRef = doc(collection(firestoreDB, "users"), user.uid);
+      // Check if the document already exists
+      const userDoc = await getDoc(userRef);
+      if (userDoc.exists()) {
+        console.log("Document already exists:", userDoc.data());
+        return; // Stop execution if the document already exists
       }
-      const unsubscribe = onAuthStateChanged(auth, (user) => {
-          if (user) {
-              setUser(user);
-          } else {
-              setUser(null);
-          }
-          setLoading(false);
+      await setDoc(userRef, {
+        name: user.displayName,
+        email: user.email,
+        photoURL: user.photoURL,
+        uid: user.uid,
+        posts: [],
+        isInGroup: [],
+        activity: [],
+        follows: [],
+        followedBy: [],
       });
+    }  
 
-      return () => unsubscribe();
-  }, []);*/
+    useEffect(() => {
+        auth.onAuthStateChanged((user: any) => {
+            setUser(user);
+            storeUserData(user);
+            console.log(user);
+            setLoadingAuth(false);
+        });
+    }, []);
 
-  useEffect(() => {
-    auth.onAuthStateChanged((user: any) => {
-      setUser(user);
-      console.log(user);
-      setLoadingAuth(false);
-    });
-  }, []);
-
-  return (
-    <AuthContext.Provider value={userValue}>
+    return (
+      <AuthContext.Provider value={userValue}>
       {loadingAuth ?
         /*<div>
           <>{View}</>
@@ -87,7 +115,5 @@ export const AuthProvider = ({ children }: any) => {
         </div>
         : children}
     </AuthContext.Provider>
-  );
+    );
 };
-
-export default { AuthProvider, useAuthContext }
